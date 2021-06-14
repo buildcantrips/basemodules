35ffba09a15980e8bb07b66900705cf304cd511d
Csaba Makara <csaba.makara@emarsys.com>
2021-06-14 12:03:55 +0200

    chore(lint): remove semicolons

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import BuildRunners from "./BuildRunners"
 import Aws from "./Aws"
 import Docker from "./Docker"
-import S3Handler from "./S3Handler"
 
 module.exports = {
   BuildRunners,
   Aws,
   Docker,
-  S3Handler,
 }
