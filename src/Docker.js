f640a256bd095740392d579fae9cbc54608acfd9
Csaba Makara <csaba.makara@emarsys.com>
2021-06-14 11:30:39 +0200

    feat(docker): add default pulling to build

--- src/Docker.js
+++ src/Docker.js
@@ -1,119 +1,155 @@
-import { Logger, ParameterProvider, ProcessUtils, StringUtils } from "@cantrips/core"
+import {
+  Logger,
+  ParameterProvider,
+  ProcessUtils,
+  StringUtils,
+} from "@cantrips/core";
 
 class Docker {
   constructor(location, commandRunner) {
-    this.location = location
-    this.runCommand = commandRunner || ProcessUtils.runCommandSync
-    this.parameterProvider = new ParameterProvider()
+    this.location = location;
+    this.runCommand = commandRunner || ProcessUtils.runCommandSync;
+    this.parameterProvider = new ParameterProvider();
   }
 
   getFirstMatchOrDefault(text, regex) {
-    const result = text.match(regex)
-    return result ? result[1] : null
+    const result = text.match(regex);
+    return result ? result[1] : null;
   }
 
   computeTagsByDockerFilesFromString(parsedImageDescriptors) {
-    const resultHash = {}
-    parsedImageDescriptors.forEach(imageDescriptor => {
-      const dockerFile = this.getFirstMatchOrDefault(imageDescriptor, /.*\[(.*)\]/) || "Dockerfile"
-      const tag = this.getFirstMatchOrDefault(imageDescriptor, /:([^[\s]*)/) || "latest"
-      const imageName = this.getFirstMatchOrDefault(imageDescriptor, /^([^:^[.]+)/) || ""
+    const resultHash = {};
+    parsedImageDescriptors.forEach((imageDescriptor) => {
+      const dockerFile =
+        this.getFirstMatchOrDefault(imageDescriptor, /.*\[(.*)\]/) ||
+        "Dockerfile";
+      const tag =
+        this.getFirstMatchOrDefault(imageDescriptor, /:([^[\s]*)/) || "latest";
+      const imageName =
+        this.getFirstMatchOrDefault(imageDescriptor, /^([^:^[.]+)/) || "";
 
       if (!StringUtils.isNormalizedString(imageName)) {
-        throw `Image name ${imageName} is not a valid docker image name.`
+        throw `Image name ${imageName} is not a valid docker image name.`;
       }
 
       if (!resultHash[dockerFile]) {
-        resultHash[dockerFile] = []
+        resultHash[dockerFile] = [];
       }
-      resultHash[dockerFile].push(`${imageName}:${tag}`)
-    })
-    return resultHash
+      resultHash[dockerFile].push(`${imageName}:${tag}`);
+    });
+    return resultHash;
   }
   async computeTagsByDockerFilesFromJson(parsedImageDescriptorJson) {
-    const resultHash = {}
-    Object.keys(parsedImageDescriptorJson).forEach(async imageName => {
+    const resultHash = {};
+    Object.keys(parsedImageDescriptorJson).forEach(async (imageName) => {
       if (!StringUtils.isNormalizedString(imageName)) {
-        throw `Image name ${imageName} is not a valid docker image name.`
+        throw `Image name ${imageName} is not a valid docker image name.`;
       }
 
-      const dockerFile = parsedImageDescriptorJson[imageName]["dockerFile"] || "Dockerfile"
+      const dockerFile =
+        parsedImageDescriptorJson[imageName]["dockerFile"] || "Dockerfile";
 
-      const tags = parsedImageDescriptorJson[imageName]["tags"] || ["latest"]
+      const tags = parsedImageDescriptorJson[imageName]["tags"] || ["latest"];
 
       if (!resultHash[dockerFile]) {
-        resultHash[dockerFile] = []
+        resultHash[dockerFile] = [];
       }
-      tags.forEach(tag => {
-        resultHash[dockerFile].push(`${imageName}:${tag}`)
-      })
-    })
-    return resultHash
+      tags.forEach((tag) => {
+        resultHash[dockerFile].push(`${imageName}:${tag}`);
+      });
+    });
+    return resultHash;
   }
 
   async computeImagesByDockerFiles(images) {
-    let imagesByDockerFiles
-    images = images || (await this.computeDefaultImageName())
+    let imagesByDockerFiles;
+    images = images || (await this.computeDefaultImageName());
     try {
-      const parsedImageDescriptors = JSON.parse(images)
-      imagesByDockerFiles = await this.computeTagsByDockerFilesFromJson(parsedImageDescriptors["docker"])
-      Logger.debug("Docker - Json input format detected")
+      const parsedImageDescriptors = JSON.parse(images);
+      imagesByDockerFiles = await this.computeTagsByDockerFilesFromJson(
+        parsedImageDescriptors["docker"]
+      );
+      Logger.debug("Docker - Json input format detected");
     } catch (e) {
-      const parsedImageDescriptors = images.split(",")
-      imagesByDockerFiles = await this.computeTagsByDockerFilesFromString(parsedImageDescriptors)
-      Logger.debug("Docker - String input format detected")
+      const parsedImageDescriptors = images.split(",");
+      imagesByDockerFiles = await this.computeTagsByDockerFilesFromString(
+        parsedImageDescriptors
+      );
+      Logger.debug("Docker - String input format detected");
     }
-    Logger.debug(`Detected: ${JSON.stringify(imagesByDockerFiles, null, 2)}`)
-    return imagesByDockerFiles
+    Logger.debug(`Detected: ${JSON.stringify(imagesByDockerFiles, null, 2)}`);
+    return imagesByDockerFiles;
   }
 
-  async build({ images, buildArgs = "", noCache = false } = {}) {
-    let imagesByDockerFiles = await this.computeImagesByDockerFiles(images)
+  async build({ images, buildArgs = "", noCache = false, pull = true } = {}) {
+    let imagesByDockerFiles = await this.computeImagesByDockerFiles(images);
     if (buildArgs) {
-      buildArgs = buildArgs.split(",").map(arg => ` --build-arg ${arg}`)
+      buildArgs = buildArgs.split(",").map((arg) => ` --build-arg ${arg}`);
     }
     for (let dockerFile of Object.keys(imagesByDockerFiles)) {
-      const tagCommandString = imagesByDockerFiles[dockerFile].join(" -t ")
+      const tagCommandString = imagesByDockerFiles[dockerFile].join(" -t ");
 
       this.runCommand(
-        `docker build -f ${dockerFile} ${noCache ? " --no-cache" : ""} -t ${tagCommandString} ${buildArgs} .`,
-        `Building docker image from dockerfile ${dockerFile} with tags ${imagesByDockerFiles[dockerFile].join(" ")}`
-      )
+        `docker build -f ${dockerFile} ${noCache ? " --no-cache" : ""} ${
+          pull ? " --pull" : ""
+        } -t ${tagCommandString} ${buildArgs} .`,
+        `Building docker image from dockerfile ${dockerFile} with tags ${imagesByDockerFiles[
+          dockerFile
+        ].join(" ")}`
+      );
     }
   }
 
   async push(options = {}) {
-    let { images, registryUrl = await this.parameterProvider.getParameter("DockerRegistry") } = options
+    let {
+      images,
+      registryUrl = await this.parameterProvider.getParameter("DockerRegistry"),
+    } = options;
 
-    const imagesByDockerFiles = await this.computeImagesByDockerFiles(images)
+    const imagesByDockerFiles = await this.computeImagesByDockerFiles(images);
 
     for (let dockerFile of Object.keys(imagesByDockerFiles)) {
       for (let image of imagesByDockerFiles[dockerFile]) {
-        this.tag(image, `${registryUrl}/${image}`)
-        this.runCommand(`docker push ${registryUrl}/${image}`, `Pushing image ${registryUrl}/${image}`)
+        this.tag(image, `${registryUrl}/${image}`);
+        this.runCommand(
+          `docker push ${registryUrl}/${image}`,
+          `Pushing image ${registryUrl}/${image}`
+        );
       }
     }
   }
 
   async tag(imageToTag, newImageName) {
-    return this.runCommand(`docker tag ${imageToTag} ${newImageName}`, `Tagging image ${imageToTag} as ${newImageName}`)
+    return this.runCommand(
+      `docker tag ${imageToTag} ${newImageName}`,
+      `Tagging image ${imageToTag} as ${newImageName}`
+    );
   }
 
   async login({ username, password, registryUrl } = {}) {
-    username = username || process.env.DOCKER_USERNAME
-    password = password || process.env.DOCKER_PASSWORD
-    registryUrl = registryUrl || process.env.DOCKER_REGISTRY
-    return this.runCommand(`docker login -u ${username} -p ${password} ${registryUrl}`, `Logging into ${registryUrl}`)
+    username = username || process.env.DOCKER_USERNAME;
+    password = password || process.env.DOCKER_PASSWORD;
+    registryUrl = registryUrl || process.env.DOCKER_REGISTRY;
+    return this.runCommand(
+      `docker login -u ${username} -p ${password} ${registryUrl}`,
+      `Logging into ${registryUrl}`
+    );
   }
 
   async computeDefaultTags() {
     return (await this.parameterProvider.getParameter("IsRelease"))
       ? [this.parameterProvider.getParameter("ReleaseVersion")]
-      : [StringUtils.normalizeString(await this.parameterProvider.getParameter("ShortHash"))]
+      : [
+          StringUtils.normalizeString(
+            await this.parameterProvider.getParameter("ShortHash")
+          ),
+        ];
   }
 
   async computeDefaultImageName() {
-    return StringUtils.normalizeString(await this.parameterProvider.getParameter("ProjectName"))
+    return StringUtils.normalizeString(
+      await this.parameterProvider.getParameter("ProjectName")
+    );
   }
 }
 
@@ -130,36 +166,39 @@ module.exports = {
             my-image:customTag => Building my-image:customTag from Dockerfile\
             my-image:someTag[Dockerfile.web] => Building my-image:someTag from Dockerfile.web\
             my-image[Dockerfile.web],my-image:someTag => Building my-image from Dockerfile.web\
-                                                         and building my-image:someTag from Dockerfile"
+                                                         and building my-image:someTag from Dockerfile",
         },
         {
           name: "buildArgs",
           description:
-            "Coma separated key value pairs are passed as build args during build. Eg.: myvar1=value1,myvar2=value2"
+            "Coma separated key value pairs are passed as build args during build. Eg.: myvar1=value1,myvar2=value2",
         },
         {
           name: "noCache",
           description: "Using noCache option",
-          flag: true
-        }
-      ]
+          flag: true,
+        },
+      ],
     },
     login: {
       description: "Logging into a Docker Registry",
       parameters: [
         {
           name: "username",
-          description: "Docker registry user name. Environment: DOCKER_USERNAME"
+          description:
+            "Docker registry user name. Environment: DOCKER_USERNAME",
         },
         {
           name: "password",
-          description: "Docker registry user password. Environment: DOCKER_PASSWORD"
+          description:
+            "Docker registry user password. Environment: DOCKER_PASSWORD",
         },
         {
           name: "registryUrl",
-          description: "The target Docker Registry url. Environment: DOCKER_REGISTRY"
-        }
-      ]
+          description:
+            "The target Docker Registry url. Environment: DOCKER_REGISTRY",
+        },
+      ],
     },
     push: {
       description: "Pushing a docker image",
@@ -171,20 +210,21 @@ module.exports = {
             my-image => Pushing my-image:latest\
             my-image:customTag => Pushing my-image:customTag\
             my-image:someTag[Dockerfile.web] => Pushing my-image:someTag\
-            my-image,my-image:someTag => Pushing my-image and pushing my-image:someTag"
+            my-image,my-image:someTag => Pushing my-image and pushing my-image:someTag",
         },
         {
           name: "registryUrl",
-          description: "The target Docker Registry url. Environment: DOCKER_REGISTRY"
-        }
-      ]
-    }
+          description:
+            "The target Docker Registry url. Environment: DOCKER_REGISTRY",
+        },
+      ],
+    },
   },
   meta: {
     name: "docker",
     description: "Building and publishing docker images",
     parameters: [],
-    type: Docker
+    type: Docker,
   },
-  Docker
-}
+  Docker,
+};
--- src/Docker.test.js
+++ src/Docker.test.js
@@ -1,262 +1,288 @@
 /* eslint-env mocha */
 
-import { Docker } from "./Docker"
-import { FileSystemUtils } from "@cantrips/core"
+import { Docker } from "./Docker";
+import { FileSystemUtils } from "@cantrips/core";
 
-import path from "path"
-import { expect } from "chai"
-import childProcess from "child_process"
-import fs from "fs"
-import osTmpdir from "os-tmpdir"
+import path from "path";
+import { expect } from "chai";
+import childProcess from "child_process";
+import fs from "fs";
+import osTmpdir from "os-tmpdir";
 
-let tempDir = path.join(osTmpdir(), "cantrips_test_dir")
+let tempDir = path.join(osTmpdir(), "cantrips_test_dir");
 
 function recreateGitRepository() {
   if (fs.existsSync(tempDir)) {
-    FileSystemUtils.deleteFolderRecursive(tempDir)
+    FileSystemUtils.deleteFolderRecursive(tempDir);
   }
-  fs.mkdirSync(tempDir)
+  fs.mkdirSync(tempDir);
   childProcess.execSync(`cd ${tempDir} &&
-    echo "from scratch \n COPY * /" >> Dockerfile`)
+    echo "from scratch \n COPY * /" >> Dockerfile`);
 }
 
 function getDockerImageList() {
   return childProcess
     .execSync('docker image ls --format "{{.Repository}}:{{.Tag}}"')
     .toString()
-    .split("\n")
+    .split("\n");
 }
 
 function snapShotDockerImages() {
   return childProcess
     .execSync('docker image ls --format "{{.ID}}"')
     .toString()
-    .split("\n")
+    .split("\n");
 }
 
 describe("docker", () => {
-  var alreadyPresentDockerImage
-  var dockerHandler
-  var validDockerImageName = "my-test-org-my-test-image"
-  var validDockerRegistry = "validDockerRegistry"
+  var alreadyPresentDockerImage;
+  var dockerHandler;
+  var validDockerImageName = "my-test-org-my-test-image";
+  var validDockerRegistry = "validDockerRegistry";
   before(() => {
-    process.env.CIRCLECI = "CIRCLECI"
-    process.env.CIRCLE_BRANCH = "validBranchName"
-    process.env.CIRCLE_PROJECT_USERNAME = "validUser"
-    process.env.CIRCLE_PROJECT_REPONAME = "validRepoName"
-    process.env.CIRCLE_SHA1 = "1234567890"
-    process.env.DOCKER_REGISTRY = validDockerRegistry
-    process.env.CIRCLE_TAG = ""
-    process.env.CIRCLE_BRANCH = "work/myBranch"
-    process.env.DOCKER_USERNAME = "validDockerUserFromEnv"
-    process.env.DOCKER_PASSWORD = "validDockerPasswordFromEnv"
-    dockerHandler = new Docker(tempDir)
-    alreadyPresentDockerImage = snapShotDockerImages()
-  })
+    process.env.CIRCLECI = "CIRCLECI";
+    process.env.CIRCLE_BRANCH = "validBranchName";
+    process.env.CIRCLE_PROJECT_USERNAME = "validUser";
+    process.env.CIRCLE_PROJECT_REPONAME = "validRepoName";
+    process.env.CIRCLE_SHA1 = "1234567890";
+    process.env.DOCKER_REGISTRY = validDockerRegistry;
+    process.env.CIRCLE_TAG = "";
+    process.env.CIRCLE_BRANCH = "work/myBranch";
+    process.env.DOCKER_USERNAME = "validDockerUserFromEnv";
+    process.env.DOCKER_PASSWORD = "validDockerPasswordFromEnv";
+    dockerHandler = new Docker(tempDir);
+    alreadyPresentDockerImage = snapShotDockerImages();
+  });
   after(() => {
     if (fs.existsSync(tempDir)) {
-      FileSystemUtils.deleteFolderRecursive(tempDir)
+      FileSystemUtils.deleteFolderRecursive(tempDir);
     }
-    var currentDockerImages = snapShotDockerImages()
+    var currentDockerImages = snapShotDockerImages();
 
     var imagesToRemove = currentDockerImages
-      .filter(image => !alreadyPresentDockerImage.includes(image))
-      .join(" ")
+      .filter((image) => !alreadyPresentDockerImage.includes(image))
+      .join(" ");
 
     if (imagesToRemove) {
-      childProcess.execSync(`docker rmi -f ${imagesToRemove}`)
+      childProcess.execSync(`docker rmi -f ${imagesToRemove}`);
     }
-  })
+  });
   beforeEach(() => {
-    recreateGitRepository()
-  })
+    recreateGitRepository();
+  });
 
   describe("computeTagsByDockerFilesFromString", () => {
     [
       {
         input: ["valid-image"],
         expectedResult: { Dockerfile: ["valid-image:latest"] },
-        description: "Default dockerfile and tag"
+        description: "Default dockerfile and tag",
       },
       {
         input: ["valid-image", "valid-image2"],
         expectedResult: {
-          Dockerfile: ["valid-image:latest", "valid-image2:latest"]
+          Dockerfile: ["valid-image:latest", "valid-image2:latest"],
         },
-        description: "Multiple images -  default dockerfile and tags"
+        description: "Multiple images -  default dockerfile and tags",
       },
       {
         input: ["valid-image:valid-tag"],
         expectedResult: { Dockerfile: ["valid-image:valid-tag"] },
-        description: "Custom tag"
+        description: "Custom tag",
       },
       {
         input: ["valid-image:valid-tag[dockerfile.valid]"],
         expectedResult: { "dockerfile.valid": ["valid-image:valid-tag"] },
-        description: "Custom tag and dockerfile"
+        description: "Custom tag and dockerfile",
       },
       {
         input: ["valid-image[dockerfile.valid]"],
         expectedResult: { "dockerfile.valid": ["valid-image:latest"] },
-        description: "Default tag and custom dockerfile"
-      }
-    ].forEach(testCase => {
+        description: "Default tag and custom dockerfile",
+      },
+    ].forEach((testCase) => {
       it(`computes results correctly: ${testCase.description}`, async () => {
         expect(
           await dockerHandler.computeTagsByDockerFilesFromString(testCase.input)
-        ).to.deep.equal(testCase.expectedResult)
-      })
-    })
-  })
+        ).to.deep.equal(testCase.expectedResult);
+      });
+    });
+  });
 
   describe("computeTagsByDockerFilesFromJson", () => {
     [
       {
         input: { "valid-image": {} },
         expectedResult: { Dockerfile: ["valid-image:latest"] },
-        description: "Default dockerfile and tag"
+        description: "Default dockerfile and tag",
       },
       {
         input: { "valid-image": {}, "valid-image2": {} },
         expectedResult: {
-          Dockerfile: ["valid-image:latest", "valid-image2:latest"]
+          Dockerfile: ["valid-image:latest", "valid-image2:latest"],
         },
-        description: "Multiple images -  default dockerfile and tags"
+        description: "Multiple images -  default dockerfile and tags",
       },
       {
         input: { "valid-image": { tags: ["valid-tag"] } },
         expectedResult: { Dockerfile: ["valid-image:valid-tag"] },
-        description: "Custom tag"
+        description: "Custom tag",
       },
       {
         input: {
-          "valid-image": { tags: ["valid-tag"], dockerFile: "dockerfile.valid" }
+          "valid-image": {
+            tags: ["valid-tag"],
+            dockerFile: "dockerfile.valid",
+          },
         },
         expectedResult: { "dockerfile.valid": ["valid-image:valid-tag"] },
-        description: "Custom tag and dockerfile"
+        description: "Custom tag and dockerfile",
       },
       {
         input: { "valid-image": { dockerFile: "dockerfile.valid" } },
         expectedResult: { "dockerfile.valid": ["valid-image:latest"] },
-        description: "Default tag and custom dockerfile"
-      }
-    ].forEach(testCase => {
+        description: "Default tag and custom dockerfile",
+      },
+    ].forEach((testCase) => {
       it(`computes results correctly: ${testCase.description}`, async () => {
         expect(
           await dockerHandler.computeTagsByDockerFilesFromJson(testCase.input)
-        ).to.deep.equal(testCase.expectedResult)
-      })
-    })
-  })
+        ).to.deep.equal(testCase.expectedResult);
+      });
+    });
+  });
+
   describe("build", () => {
     it("docker images can be built with image name parameter", async () => {
-      await dockerHandler.build({ images: validDockerImageName })
-      expect(getDockerImageList()).to.include(`${validDockerImageName}:latest`)
-    })
+      await dockerHandler.build({ images: validDockerImageName });
+      expect(getDockerImageList()).to.include(`${validDockerImageName}:latest`);
+    });
 
     it("docker images can be built with multiple image name parameter", async () => {
-      const customTag = "customTag"
+      const customTag = "customTag";
       await dockerHandler.build({
-        images: `${validDockerImageName},${validDockerImageName}:${customTag}`
-      })
-      expect(getDockerImageList()).to.include(`${validDockerImageName}:latest`)
+        images: `${validDockerImageName},${validDockerImageName}:${customTag}`,
+      });
+      expect(getDockerImageList()).to.include(`${validDockerImageName}:latest`);
       expect(getDockerImageList()).to.include(
         `${validDockerImageName}:${customTag}`
-      )
-    })
+      );
+    });
 
     it("default docker image name is used on not setting it as parameter", async () => {
-      await dockerHandler.build()
-      expect(getDockerImageList()).to.include("validuser-validreponame:latest")
-    })
-  })
+      await dockerHandler.build();
+      expect(getDockerImageList()).to.include("validuser-validreponame:latest");
+    });
+    describe("build - pull", () => {
+      var results = [];
+      var dockerHandler;
+
+      beforeEach(() => {
+        results = [];
+        dockerHandler = new Docker(tempDir, (command) => results.push(command));
+      });
+      it("docker build pulls from image by default", async () => {
+        await dockerHandler.build({ images: validDockerImageName });
+        expect(results[0]).to.include(`--pull`);
+      });
+
+      it("docker build does not pull from image if it is turned off", async () => {
+        await dockerHandler.build({
+          images: validDockerImageName,
+          pull: false,
+        });
+        expect(results[0]).to.not.include(`--pull`);
+      });
+    });
+  });
   describe("computeDefaultTags", () => {
     it("gives back normalized branch name in build mode", async () => {
-      expect(await dockerHandler.computeDefaultTags()).to.include("12345678")
-    })
+      expect(await dockerHandler.computeDefaultTags()).to.include("12345678");
+    });
     it("gives back release version in release mode", async () => {
-      process.env.CIRCLE_TAG = "release-1.2.3"
-      var dockerHandler = new Docker(tempDir, command => command)
-      process.env.CIRCLE_TAG = ""
-      expect(await dockerHandler.computeDefaultTags()).to.include("1.2.3")
-    })
-  })
+      process.env.CIRCLE_TAG = "release-1.2.3";
+      var dockerHandler = new Docker(tempDir, (command) => command);
+      process.env.CIRCLE_TAG = "";
+      expect(await dockerHandler.computeDefaultTags()).to.include("1.2.3");
+    });
+  });
 
   describe("computeDefaultImageName", () => {
     it("computes correct image name", async () => {
       expect(await dockerHandler.computeDefaultImageName()).to.equal(
         "validuser-validreponame"
-      )
-    })
-  })
+      );
+    });
+  });
 
   describe("login", () => {
-    var validUser = "validUser"
-    var validPassword = "validPassword"
+    var validUser = "validUser";
+    var validPassword = "validPassword";
     it("uses given username and password to authenticate", async () => {
-      var dockerHandler = new Docker(tempDir, command => command)
+      var dockerHandler = new Docker(tempDir, (command) => command);
       var result = await dockerHandler.login({
         username: validUser,
-        password: validPassword
-      })
-      expect(result).to.contain(`-u ${validUser} -p ${validPassword}`)
-    })
+        password: validPassword,
+      });
+      expect(result).to.contain(`-u ${validUser} -p ${validPassword}`);
+    });
 
     it("if parameters absent, uses environment defaults", async () => {
-      var dockerHandler = new Docker(tempDir, command => command)
-      var result = await dockerHandler.login()
+      var dockerHandler = new Docker(tempDir, (command) => command);
+      var result = await dockerHandler.login();
       expect(result).to.contain(
         `-u validDockerUserFromEnv -p validDockerPasswordFromEnv`
-      )
-    })
-  })
+      );
+    });
+  });
 
   describe("tag", () => {
     it("tags an image with new tag...", async () => {
-      var imageName = "image-to-tag3"
-      var newTag = "new-tag"
-      await dockerHandler.build({ images: imageName })
-      await dockerHandler.tag(`${imageName}:latest`, `${imageName}:${newTag}`)
-      expect(getDockerImageList()).to.contain(`${imageName}:${newTag}`)
-    })
-  })
+      var imageName = "image-to-tag3";
+      var newTag = "new-tag";
+      await dockerHandler.build({ images: imageName });
+      await dockerHandler.tag(`${imageName}:latest`, `${imageName}:${newTag}`);
+      expect(getDockerImageList()).to.contain(`${imageName}:${newTag}`);
+    });
+  });
+
   describe("push", () => {
-    var results = []
-    var dockerHandler
+    var results = [];
+    var dockerHandler;
 
     before(() => {
-      dockerHandler = new Docker(tempDir, command => results.push(command))
+      dockerHandler = new Docker(tempDir, (command) => results.push(command));
       dockerHandler.logger = {
-        info: msg => {
-          results.push(msg)
-        }
-      }
-    })
+        info: (msg) => {
+          results.push(msg);
+        },
+      };
+    });
 
     beforeEach(() => {
-      results = []
-    })
+      results = [];
+    });
 
     it("pushes the default latest image if parameters are not present", async () => {
-      await dockerHandler.push()
+      await dockerHandler.push();
       expect(results).to.include(
         `docker push ${validDockerRegistry}/validuser-validreponame:latest`
-      )
-    })
+      );
+    });
     it("pushes the specified tag of target image if given", async () => {
       await dockerHandler.push({
         images: JSON.stringify({
           docker: {
             "validuser-validreponame": {
-              tags: ["myTag"]
-            }
-          }
-        })
-      })
+              tags: ["myTag"],
+            },
+          },
+        }),
+      });
 
       expect(results).to.include(
         `docker push ${validDockerRegistry}/validuser-validreponame:myTag`
-      )
-    })
-  })
-})
+      );
+    });
+  });
+});
