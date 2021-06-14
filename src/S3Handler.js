bf9f9668b3969c39c65716e29e9426a66f4538dd
Csaba Makara <csaba.makara@emarsys.com>
2021-06-14 11:30:18 +0200

    chore(dusting): removed old component, fix tests

--- src/S3Handler.js
+++ src/S3Handler.js
@@ -1,56 +1,53 @@
-import { ContainerProvider, Logger } from "@cantrips/core"
+import { ContainerProvider, Logger } from "@cantrips/core";
 
 class S3Handler {
   constructor({ accessKeyId, secretAccessKey }) {
-    this.imageUrl = "garland/aws-cli-docker"
-    this.container = undefined
-    this.accessKeyId = accessKeyId || process.env.AWS_ACCESS_KEY_ID
-    this.secretAccessKey = secretAccessKey || process.env.AWS_SECRET_ACCESS_KEY
+    this.imageUrl = "garland/aws-cli-docker";
+    this.container = undefined;
+    this.accessKeyId = accessKeyId || process.env.AWS_ACCESS_KEY_ID;
+    this.secretAccessKey = secretAccessKey || process.env.AWS_SECRET_ACCESS_KEY;
     if (!this.accessKeyId || !this.secretAccessKey) {
       throw Error(
         "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment parameters are mandatory"
-      )
+      );
     }
   }
 
   async init() {
-    this.container = await ContainerProvider(this.imageUrl)
+    this.container = await ContainerProvider(this.imageUrl);
     this.container.addEnvironmentVariable(
       "AWS_ACCESS_KEY_ID",
       this.accessKeyId
-    )
+    );
     this.container.addEnvironmentVariable(
       "AWS_SECRET_ACCESS_KEY",
       this.secretAccessKey
-    )
+    );
   }
 
   async list({ bucketName }) {
     await this.container.run(
       `aws s3 ls ${bucketName}`,
       `Listing bucket ${bucketName}`
-    )
+    );
   }
 
   async get({ fileUri, targetPath }) {
     if (!fileUri.startsWith("s3://")) {
-      throw Error('First parameter must start with "s3://"')
+      throw Error('First parameter must start with "s3://"');
     }
     if (!targetPath) {
-      targetPath = `./${fileUri
-        .split("/")
-        .slice(-1)
-        .pop()}`
+      targetPath = `./${fileUri.split("/").slice(-1).pop()}`;
     }
-    Logger.info(`Downloading file from ${fileUri} to ${targetPath}`)
-    await this.container.run(`aws s3 cp ${fileUri} ${targetPath}`, "")
+    Logger.info(`Downloading file from ${fileUri} to ${targetPath}`);
+    await this.container.run(`aws s3 cp ${fileUri} ${targetPath}`, "");
   }
 }
 
 async function wrapper(args) {
-  const handler = new S3Handler(args)
-  await handler.init()
-  return handler
+  const handler = new S3Handler(args);
+  await handler.init();
+  return handler;
 }
 
 module.exports = {
@@ -60,9 +57,9 @@ module.exports = {
       parameters: [
         {
           name: "bucketName",
-          description: "AWS S3 bucket name to list. (Mandatory)"
-        }
-      ]
+          description: "AWS S3 bucket name to list. (Mandatory)",
+        },
+      ],
     },
     get: {
       description: "",
@@ -70,15 +67,15 @@ module.exports = {
         {
           name: "fileUri",
           description:
-            "The S3 uri of the target object to download. (Mandatory)"
+            "The S3 uri of the target object to download. (Mandatory)",
         },
         {
           name: "targetPath",
           description:
-            "The local path to download the target object to. (Mandatory)"
-        }
-      ]
-    }
+            "The local path to download the target object to. (Mandatory)",
+        },
+      ],
+    },
   },
   meta: {
     name: "s3",
@@ -87,15 +84,15 @@ module.exports = {
       {
         name: "accessKeyId",
         description:
-          "AWS AccessKeyID to authenticate. Environment: AWS_ACCESS_KEY_ID. (Mandatory)"
+          "AWS AccessKeyID to authenticate. Environment: AWS_ACCESS_KEY_ID. (Mandatory)",
       },
       {
         name: "secretAccessKey",
         description:
-          "AWS SecretAccessKey to authenticate. Environment: AWS_SECRET_ACCESS_KEY. (Mandatory)"
-      }
+          "AWS SecretAccessKey to authenticate. Environment: AWS_SECRET_ACCESS_KEY. (Mandatory)",
+      },
     ],
-    type: wrapper
+    type: wrapper,
   },
-  S3Handler: wrapper
-}
+  S3Handler: wrapper,
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import BuildRunners from "./BuildRunners"
-import Aws from "./Aws"
-import Docker from "./Docker"
-import ElasticBeanstalk from "./ElasticBeanstalk"
-import S3Handler from "./S3Handler"
+import BuildRunners from "./BuildRunners";
+import Aws from "./Aws";
+import Docker from "./Docker";
+import S3Handler from "./S3Handler";
 
 module.exports = {
   BuildRunners,
   Aws,
   Docker,
-  ElasticBeanstalk,
-  S3Handler
-}
+  S3Handler,
+};
